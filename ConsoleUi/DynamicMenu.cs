9a5ebed166f1ead9ac3759bea0ad67b9a43dc791
Antoine Aubry <antoine@gothelist.com>
2018-11-07 17:16:55 +0000

    Add DynamicMenu overloads

--- ConsoleUi/DynamicMenu.cs
+++ ConsoleUi/DynamicMenu.cs
@@ -1,15 +1,15 @@
 // This file is part of ConsoleUi.
-// 
+//
 // ConsoleUi is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // ConsoleUi is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with ConsoleUi.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -31,6 +31,17 @@ namespace ConsoleUi
 
         public DynamicMenu(string title, Func<IMenuContext, Task<IEnumerable<IMenuItem>>> getItems)
             : this(title, ctx => getItems(ctx).Result)
+        {
+        }
+
+        public DynamicMenu(string title, bool executeIfSingleItem, Func<IMenuContext, IEnumerable<IMenuItem>> getItems)
+            : base(title, executeIfSingleItem)
+        {
+            this.getItems = getItems;
+        }
+
+        public DynamicMenu(string title, bool executeIfSingleItem, Func<IMenuContext, Task<IEnumerable<IMenuItem>>> getItems)
+            : this(title, executeIfSingleItem, ctx => getItems(ctx).Result)
         {
         }
 
